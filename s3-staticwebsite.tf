a2a2a8b8082b787e603de4b10cb70462d70769bc
Suman Rizvi <sumanrizvi@gmail.com>
2022-11-11 15:50:58 -0800

    making static website

---
+++ s3-staticwebsite.tf
@@ -0,0 +1,31 @@
+resource "aws_s3_bucket" "s3staticwebsitebucket" {
+    bucket = "rizvis3staticwebsitelesson"
+}
+
+resource "aws_s3_bucket_object" "s3staticwebsiteindex" {
+    bucket = aws_s3_bucket.s3staticwebsitebucket.id
+    key = "index.html"
+    source = "./s3-statiswebsite-lesson/index.html"
+    acl = "public-read"
+    content_type = "text/html"
+}
+
+resource "aws_s3_bucket_object" "s3staticwebsiteerror" {
+    bucket = aws_s3_bucket.s3staticwebsitebucket.bucket
+    key = "error.html"
+    source = "./s3-statiswebsite-lesson/error.html"
+    acl = "public-read"
+    content_type = "text/html"
+}
+
+resource "aws_s3_bucket_website_configuration" "s3staticwebsiteconfiguration" {
+    bucket = aws_s3_bucket.s3staticwebsitebucket.bucket
+
+    index_document {
+        suffix = "index.html"
+    }
+
+    error_document {
+        key = "error.html"
+    }
+}
